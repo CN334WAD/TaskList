dc20e6543f4f20a957cd23fb9abab6a7983b754b
WonYus <lucy_oliv@outlook.com>
2022-03-18 13:44:20 +0700

    reste default

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -26,6 +26,6 @@ class ExampleTest extends TestCase
             'password' => "testpassword"
         ]);
 
-        $this->assertEquals('TestUser', $user->name);
+        $this->assertEquals('Test User', $user->name);
     }
 }
