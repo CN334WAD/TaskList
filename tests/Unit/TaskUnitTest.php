7117ff5caf53f1a85c40dc0049cd9e22fd3c1409
WonYus <lucy_oliv@outlook.com>
2022-03-23 12:33:54 +0700

    update test

--- tests/Unit/TaskUnitTest.php
+++ tests/Unit/TaskUnitTest.php
@@ -3,23 +3,71 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Models\Task;
+use Illuminate\Support\Facades\Schema;
+
+
+
 
 class TaskUnitTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testcreatetask()
+
+    // test schema ทดสอบ description ถูกต้อง
+    public function test_schema_description()
     {
-        $task = new Task();
-        $task->description = 'Test Task';
-        $task->user_id = 1;
         
-        $this->assertEquals('Test Task', $task->description);
+        $this->assertTrue(
+            Schema::hasTable('users')
+        );
+        // $this->assertTrue( 
+        //     Schema::hasColumns('users', [
+        //       'id','name', 'email', 'email_verified_at', 'password'
+        //   ]), 1);
     }
 
-    
+    // test schema ทดสอบ user_id ถูกต้อง
+    public function test_schema_user_id()
+    {
+
+        $this->assertTrue(
+            Schema::hasColumn('tasks', 'user_id')
+        );
+    }
+
+    //test schema ทดสอบ รองรับภาษาไทยไหม
+    public function test_description_thai_language()
+    {
+        // $this->assertDatabaseHas('tasks', [
+        //     'description' => 'ภาษาไทย'
+        // ]);
+    }
+
+    //test schema ทดสอบ รองรับภาษาอังกฤษได้ไหม
+    public function test_description_english_language()
+    {
+    }
+
+    //test schema ทดสอบ ใส่ตัวเลขอย่างเดียวได้ไหม
+    public function test_description_numeric()
+    {
+    }
+
+    //test schema ทดสอบ คำสั่ง SQL ได้ไหม
+    public function test_description_sql_query()
+    {
+    }
+
+    //test schema ทดสอบ เป็น null ได้ไหม
+    public function test_description_null()
+    {
+    }
+
+    //test schema ทดสอบ เป็น empty string ได้ไหม
+    public function test_description_empty_string()
+    {
+    }
+
+    //test schema ทดสอบ เป็น integer.max+1 ได้ไหม
+    public function test_description_integer_max()
+    {
+    }
 }
---
+++ tests/Unit/UserUnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\User;
+
+class UserUnitTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+   
+
+    # unit test of name email pass
+    public function testUserCreation()
+    {
+        
+    }
+}
